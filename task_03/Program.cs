be653534d40ed7937b0f981f9e694f1d45425ca5
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-18 17:41:09 +0700

    Задача 3

---
+++ task_03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message);        //Вывод приглашения
+    string strValue;                      //Объявляем переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; //Вводим строку с консоли (можно ввести только строку)
+    int value = int.Parse(strValue);      //Преобразование строки в целое число
+    return value;
+}
+
+int Value = Prompt("Введите число > ");
+int Value1 = Value % 2;
+if(Value1 == 0)
+{
+System.Console.WriteLine($"{Value} -> да");
+}
+else
+{
+System.Console.WriteLine($"{Value} -> нет");
+}
