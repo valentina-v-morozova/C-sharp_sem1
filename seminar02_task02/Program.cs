6ab7315ef119ef26625bd39083e28ec6c9598735
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 15:47:58 +0700

    Семинар 2, ДЗ 2

---
+++ seminar02_task02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числa
+
+
+int randValue = new Random().Next(10, 1000); // выводим случайное значение
+int newValue = 0;
+
+newValue = (randValue / 100) * 10 + (randValue % 10);
+
+Console.WriteLine($"{randValue} -> {newValue}");
