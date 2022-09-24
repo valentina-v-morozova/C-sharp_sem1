8562a426b0b0114e3ad9f600674276e570ab8efe
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 16:13:30 +0700

    Семинар 2, ДЗ 3

---
+++ seminar02_task03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int userNumber = Prompt("Введите число > ");
+
+if (userNumber < 100)
+{
+Console.WriteLine($"{userNumber} -> в этом числе нет 3 цифры");
+}
+else
+{
+    int newNumber = userNumber;
+    while (newNumber > 1000)
+    {
+        newNumber = newNumber / 10;
+    }
+    int thirdNumber = newNumber % 10;
+    Console.WriteLine($"{userNumber} -> {thirdNumber}");
+}
