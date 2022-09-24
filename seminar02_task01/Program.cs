51510ac49531579c6ffdf040773d8752b42e9184
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 15:40:09 +0700

    Семинар 2, ДЗ 1

---
+++ seminar02_task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int userNumber = Prompt("Введите трехзначное число > ");
+if (userNumber < 1000 && userNumber > 99)
+{
+    int result = (userNumber / 10) % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
