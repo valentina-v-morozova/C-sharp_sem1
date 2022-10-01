e754a5c22bc1dd21ef756f16c5a18418958e8765
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-10-01 17:43:20 +0700

    семинар 4 задача 3

---
+++ seminar04_task03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный случайными числами. Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
+//  -> 8
+
+int[] Create(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 50);
+    }
+    return array;
+}
+
+void DisplayArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+DisplayArray(Create(8));
