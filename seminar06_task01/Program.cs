caac625bb912d97b64cecfb2179b3c4987fbfc23
Valentina V. Trunova <valentina.v.trunova@yandex.ru>
2022-10-05 15:11:47 +0700

    семинар 6 задача 1

---
+++ seminar06_task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите элементы (через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+System.Console.Write($" -> {count}");
