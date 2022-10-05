484e7815948e0015016c20efef9200eba986bab7
Valentina V. Trunova <valentina.v.trunova@yandex.ru>
2022-10-05 20:10:48 +0700

    семинар 9 задача 1

---
+++ seminar09_task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = Prompt("Введите значение N > ");
+int count = 1;
+NaturalToLow(number, count);
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели неверное значение");
+}
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write($"{count}, ");
+    }
+}
