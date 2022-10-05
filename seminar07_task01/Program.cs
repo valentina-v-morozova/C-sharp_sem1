573f22cf72e1fae159a6a16fd4bfa4955953f935
Valentina V. Trunova <valentina.v.trunova@yandex.ru>
2022-10-05 15:48:57 +0700

    семинар 7 задача 1

---
+++ seminar07_task01/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+const int MIN_ELEMENTS = -100;
+const int MAX_ELEMENTS = 100;
+
+int length = Prompt("Введите количество столбцов > ");
+int height = Prompt("Введите количество строк > ");
+double[,] numbers = new double[height, length];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(MIN_ELEMENTS, MAX_ELEMENTS)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine("");
+    }
+}
